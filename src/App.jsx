746be22c19521efb72d69d5ecca242b18fc83699
Victor Massarenti <victormassarenti@gmail.com>
2022-09-27 01:26:44 +0000

    projeto estático finalizado

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,29 @@
+import { Header } from "./components/Header"
+import { Sidebar } from "./components/Sidebar"
+import { Post } from "./components/Post"
 
+import styles from './App.module.css'
+
+import './global.css'
 
 function App() {
 
   return (
-    <h1>Hello World</h1>
+    <div>
+      <Header />
+      <div className={styles.wrapper}>
+        <Sidebar />
+
+        <main>
+
+          <Post author="Victor Massarenti" content="Exemplo de conteúdo bla bla bla bla" />
+          <Post author="Massarenti Victor" content="Outro exemplo de conteúdo diferente do acima" />
+          <Post author="Eduardo Massarenti" content="Mais um exemplo divergente dos exemplos acima" />
+
+        </main>
+      </div>
+
+    </div>
   )
 }
 
---
+++ src/components/Comment.jsx
@@ -0,0 +1,35 @@
+import { ThumbsUp, Trash } from "phosphor-react";
+import { Avatar } from "./Avatar";
+import styles from './Comment.module.css'
+
+export function Comment() {
+    return (
+        <div className={styles.comment}>
+            <Avatar hasBorder={false} src="https://github.com/diego3g.png" />
+
+            <div className={styles.commentBox}>
+                <div className={styles.commentContent}>
+                    <header>
+                        <div className={styles.authorAndTime}>
+                            <strong>Diego Fernandes</strong>
+                            <time title='11 de maio às 08:13h' dataTime='2022-05-11 08:13:30'>Cerca de 1h atrás</time>
+                        </div>
+
+                        <button title="Deletar comentário">
+                            <Trash size={24} />
+                        </button>
+                    </header>
+
+                    <p>Muito Bom Devon, parabéns!! 👏👏</p>
+                </div>
+
+                <footer>
+                    <button>
+                        <ThumbsUp />
+                        Aplaudir <span>20</span>
+                    </button>
+                </footer>
+            </div>
+        </div>
+    )
+}
